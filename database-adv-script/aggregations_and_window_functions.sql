42306d0ee34fe8eae59194d34069da6881b44b5b
4bd3lh4m1d B3l <hamadaizou@outlook.com>
2025-09-13 15:00:57 +0100

    add a change

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -20,3 +20,13 @@ LEFT JOIN Booking b
 GROUP BY p.property_id, p.name
 ORDER BY booking_rank;
 
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM Property p
+LEFT JOIN Booking b 
+    ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY booking_rank;
